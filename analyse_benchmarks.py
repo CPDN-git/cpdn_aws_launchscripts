7f8c48bbc71bc9e5b16130639106b0c2342def39
Peter Uhe <peter.uhe@ouce.ox.ac.uk>
2016-06-01 18:17:02 +0100

    add in spot fleet launcher and rearrange functions in analyse_benchmarks script

--- analyse_benchmarks.py
+++ analyse_benchmarks.py
@@ -14,11 +14,12 @@ from aws_pythonsdk_benchmarks import get_instance_info
 
 
 
-def plot_timings():
+def plot_timings(types,runs):
 	### Plot up the timings
 	# 
 	plt.figure()
 	ax=plt.gca()
+
 
 	#for type, instances in runs.iteritems():
 	for i,type in enumerate(types):
@@ -29,12 +30,13 @@ def plot_timings():
 			plt.plot([i]*len(times),times,'b.')
 			plt.plot([i],[times.max()],'rx',mew=3)
 		
+	typex=range(len(types))
 	plt.xticks(typex,rotation=90)
 	plt.ylabel('run time (minutes)')
 	ax.set_xticklabels(types)
 	plt.show()
 
-def write_timings(csv_fname):
+def write_timings(types,runs,instance_price,csv_fname):
 	with open(csv_fname, 'wb') as csvfile:
 		csvwriter = csv.writer(csvfile)
 		csvwriter.writerow(['Instance type','hourly price','vCPUs','run time (min)','model days per instance hour','price per year run']) 
@@ -52,50 +54,75 @@ def write_timings(csv_fname):
 			csvwriter.writerow([type,hourly_price,vcpus,mean_timing,runs_per_hour,year_run_price])
 
 
+
+def get_benchmarks(instance_price=False,new_benchmarks=False):
 ### Check for any new benchmark tests ###
 #
-#os.system('aws s3 sync s3://benchmarkinglogs ../benchmarkinglogs')
-#os.system('grep -r real ../benchmarkinglogs/wah2_eu2_g61b_1966_1_short/*/timing*.txt >timing_list.txt')
 
-runs={}
+	if not instance_price:
+		instance_price=get_instance_info()
 
-### Loop over each workunit to generate the dictionary of timings
-#
-with open('timing_list.txt','r') as f:
-	for line in f:
-		instance,timing=line.split()
+	if new_benchmarks:
+		os.system('aws s3 sync s3://benchmarkinglogs ../benchmarking/benchmarkinglogs')
+		os.system('grep -r real ../benchmarking/benchmarkinglogs/wah2_eu2_g61b_1966_1_short/*/timing*.txt >../benchmarking/timing_list.txt')
+
+	runs={}
+
+	### Loop over each workunit to generate the dictionary of timings
+	#
+	with open('../benchmarking/timing_list.txt','r') as f:
+		for line in f:
+			instance,timing=line.split()
 		
-		# Get run time in minutes
-		# Assume timing in the format XXmYY.ZZZs
-		min,sec=timing.split('.')[0].split('m')
-		minutes=int(min)+int(sec)/60.
+			# Get run time in minutes
+			# Assume timing in the format XXmYY.ZZZs
+			min,sec=timing.split('.')[0].split('m')
+			minutes=int(min)+int(sec)/60.
 		
-		# Get instance info
-		arr=instance.split('/')
-		run_name=arr[1]
-		instance_type=arr[2]
-		tmp=arr[3].split('_')
-		instance_id=tmp[1]
-		instance_proc=tmp[2].split('.')[0]
+			# Get instance info
+			arr=instance.split('/')
+			run_name=arr[1]
+			instance_type=arr[2]
+			tmp=arr[3].split('_')
+			instance_id=tmp[1]
+			instance_proc=tmp[2].split('.')[0]
 		
-		# Create dictionary of run lengths
-		if not runs.has_key(instance_type):
-			runs[instance_type]={}
-		# Append this workunit to the list of runs for this instance (or create a new list)
-		try:
-			runs[instance_type][instance_id]=np.append(runs[instance_type][instance_id],minutes)
-		except:
-			runs[instance_type][instance_id]=np.array([minutes])
+			# Create dictionary of run lengths
+			if not runs.has_key(instance_type):
+				runs[instance_type]={}
+			# Append this workunit to the list of runs for this instance (or create a new list)
+			try:
+				runs[instance_type][instance_id]=np.append(runs[instance_type][instance_id],minutes)
+			except:
+				runs[instance_type][instance_id]=np.array([minutes])
 			
 
-	types=runs.keys()
-	types.sort()
-	typex=range(len(types))
+		types=runs.keys()
+		types.sort()
+
+		return types,runs
+		
+def get_weights(types,runs):
+	# At the moment use runs per hour as weighting factor
+	# (number of model days run by one instance in one hour)
+	weights={}
+	for type in types:
+		vcpus=0
+		mean_timing=0.
+		n=len(runs[type])
+		for id,timings in runs[type].iteritems():
+			vcpus=max(vcpus,len(timings))
+			mean_timing+=timings.max()
+		mean_timing=mean_timing/n
+		runs_per_hour=vcpus/mean_timing*60.
+		weights[type]=runs_per_hour
+	return weights
 
 
-
-instance_price=get_instance_info()
-
-write_timings('instance_comparison.csv')
-#plot_timings()
+if __name__=='__main__':
+	instance_price=get_instance_info()
+	types,runs=get_benchmarks()
+	write_timings(types,runs,instance_price,'instance_comparison.csv')
+	print get_weights(types,runs)
+#plot_timings(types,runs)
 		
---
+++ aws_pythonsdk_spot_fleet.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# Script to do stuff with the python AWS API, boto3
+import boto3
+from datetime import datetime,timedelta
+import os
+import base64
+from aws_pythonsdk_benchmarks import get_instance_info
+from analyse_benchmarks import get_benchmarks,get_weights
+from copy import deepcopy
+
+client = boto3.client('ec2')
+
+# This function launches one or more EC2 instance 
+# using the input parameters provided
+# Submits request though the AWS CLI (TODO could do this natively in python boto3 API)
+def launch_fleet(instance_types,targetCapacity,bootscript,dry_run=False):
+
+	print "Getting instance info..."
+	instance_price=get_instance_info()
+	print "Got prices"
+	types,runs=get_benchmarks()
+	print "Got Benchmarks"
+	weights=get_weights(types,runs)
+	print "Got weighting factors"
+
+	# Dictionary of availability zones and the subnet to use for each. 
+	subnet_map={'us-east-1a':'subnet-5a4b8b03',
+	'us-east-1c':'subnet-f08a69db',
+	'us-east-1d':'subnet-ec95349b',
+	'us-east-1e':'subnet-b58e2988'}
+
+	# List of Volume Sizes needed (dependent on number of CPUs):
+	# NOTE: commented out instances are not available with HVM virtualisation 
+	#	and would require a different submission process
+	volume_size={'c4.large':8,
+			'c4.xlarge':8,
+			'c4.2xlarge':8,
+			'c4.4xlarge':12,
+			'c4.8xlarge':24,
+			'm4.large':8,
+			'm4.xlarge':8,
+			'm4.2xlarge':8,
+			'm4.4xlarge':12,
+			'm4.10xlarge':30,
+			'm3.medium':8,
+			'm3.large':8,
+			'm3.xlarge':8,
+			'm3.2xlarge':8,
+			'c3.large':8,
+			'c3.xlarge':8,
+			'c3.2xlarge':8,
+			'c3.4xlarge':12,
+			'c3.8xlarge':24,
+			'r3.large':8,
+			'r3.xlarge':8,
+			'r3.2xlarge':8,
+			'r3.4xlarge':12,
+			'r3.8xlarge':24,
+			'i2.xlarge':8,
+			'i2.2xlarge':8,
+			'i2.4xlarge':12,
+			'i2.8xlarge':24,
+#			'm1.small':8,
+#			'm1.medium':8,
+#			'm1.large':8,
+#			'm1.xlarge':8,
+#			'c1.medium':8,
+#			'c1.xlarge':8,
+			'cc2.8xlarge':24,
+#			'm2.xlarge':8,
+#			'm2.2xlarge':8,
+#			'm2.4xlarge':12,
+			'cr1.8xlarge':24,
+			'hi1.4xlarge':12,
+#			'hs1.8xlarge':12,
+#			't1.micro':8
+	}
+	
+	launch_template={
+		"ImageId": "ami-fce3c696",
+		"InstanceType": "c3.large",
+		"KeyName": "pu_key",
+		"EbsOptimized": False,
+		"WeightedCapacity": 2,
+		"SpotPrice": "0.0525",
+		"IamInstanceProfile": {"Arn": "arn:aws:iam::677365467450:instance-profile/BenchmarkingLogs"},
+		"BlockDeviceMappings": [
+			{
+				"DeviceName": "/dev/sda1",
+				"Ebs": {
+					"DeleteOnTermination": True,
+					"VolumeType": "gp2",
+					"VolumeSize": 8,
+					"SnapshotId": "snap-f70deff0"
+				}
+			}
+		],
+		"Placement": {"AvailabilityZone": "us-east-1a"},
+		"UserData": "IyEvYmluL2Jhc2gKIyBCb290IHNjcmlwdCBmb3IgQVdTIFVidW50dSBWTSB0byBydW4gQ1BETiBydW5zIHRocm91Z2ggYm9pbmMKCiMgSW5zdGFsbCByZXF1aXJlZCBwYWNrYWdlcyBmb3IgVWJ1bnR1CnN1ZG8gYXB0LWdldCB1cGRhdGUKc3VkbyBhcHQtZ2V0IC15IGluc3RhbGwgYXdzY2xpIGxpYjMyc3RkYysrNiBsaWIzMnoxIGJvaW5jCgojIFByaW50IGRhdGUgdG8gc2VlIGhvdyBsb25nIHRoaXMgaGFzIHRha2VuCmRhdGUKCiMgQ29ubmVjdCB0byBib2luYyBwcm9qZWN0CmNkIC92YXIvbGliL2JvaW5jLWNsaWVudAojIE5vdGUsIHVzZSB3ZWFrIGtleSBoZXJlIHdoaWNoIGFsbG93cyBjb25uZWN0aW5nIHRvIHRoZSBhY2NvdW50IGJ1dCBubyBvdGhlciBwcml2aWxlZGdlcwovdXNyL2Jpbi9ib2luY2NtZCAtLXByb2plY3RfYXR0YWNoICBjbGltYXRlcHJlZGljdGlvbi5uZXQgNzE5MDkwXzI3NjdhZjMwMTFiMjFiMzVmMzkzMzA2NWQ3YzJmMzE3CgojIFdhaXQgNjUgbWludXRlcyB0byBhbGxvdyBib2luYyB0byBnZXQgdGFza3MgKGJlY2F1c2Ugb25lIGhvdXIgdGltZW91dCkKIyBUaGVuIHByZXZlbnQgYm9pbmMgZnJvbSBnZXR0aW5nIG5ldyB3b3JrCnNsZWVwIDM5MDAgCi91c3IvYmluL2JvaW5jY21kIC0tcHJvamVjdCAgY2xpbWF0ZXByZWRpY3Rpb24ubmV0IG5vbW9yZXdvcmsKCiMgV2FpdCB1bnRpbCBtb2RlbCBydW5zIGhhdmUgZmluaXNoZWQKIyBOT1RFOiB0aGlzIGlzIGhhcmRjb2RlZCB0byBjaGVjayBmb3Igd2FoMgplY2hvICJQb2xsaW5nIHdoZXRoZXIgc2NyaXB0IGlzIHN0aWxsIHJ1bm5pbmciCndoaWxlIHBzIC1lIHwgZ3JlcCB3YWgyOyBkbwogICAgICAgIHNsZWVwICA2MCAjIFdhaXQgYSBtaW51dGUKZG9uZQoKIyBDbGVhbiB1cCBhbmQgc2h1dCBkb3duCi91c3IvYmluL2JvaW5jY21kIC0tcHJvamVjdCAgY2xpbWF0ZXByZWRpY3Rpb24ubmV0IGRldGFjaApzbGVlcCA1CnN1ZG8gc2h1dGRvd24gLWggbm93Cg==",
+		"NetworkInterfaces": [
+			{
+				"DeviceIndex": 0,
+				"SubnetId": "subnet-5a4b8b03",
+				"DeleteOnTermination": True,
+				"AssociatePublicIpAddress": True,
+				"Groups": [
+					"sg-32b55654"
+				]
+			}
+		]
+	}
+	
+	# Convert boot script into base64 data
+	with open(bootscript,'r') as f:
+		userdata=base64.b64encode(f.read())
+	
+	launch_specs=[]
+	# Loop over instance types and 
+	for instance_type in instance_types:
+		print instance_type
+		for az,subnet in subnet_map.iteritems():
+			print subnet
+			launch_spec=deepcopy(launch_template) 
+			launch_spec["InstanceType"]=instance_type
+			launch_spec["WeightedCapacity"]=weights[instance_type]/weights["c4.large"]*2.
+			launch_spec["SpotPrice"]=str(instance_price[instance_type]*5.)
+			launch_spec["BlockDeviceMappings"][0]["Ebs"]["VolumeSize"]=volume_size[instance_type]
+			launch_spec["Placement"]["AvailabilityZone"]=az
+			launch_spec["NetworkInterfaces"][0]["SubnetId"]=subnet
+			launch_spec["UserData"]=userdata
+			launch_specs.append(launch_spec)
+			
+	print launch_specs
+	
+	SpotFleetRequestConfig={
+		"IamFleetRole": "arn:aws:iam::677365467450:role/aws-ec2-spot-fleet-role",
+		"AllocationStrategy": "lowestPrice",
+		"TargetCapacity": targetCapacity,
+		"SpotPrice": "0.0525",
+#		"ValidFrom": datetime.now()+timedelta(0,10), #Start 10 seconds from now 
+#		"ValidUntil": str(datetime.now()+timedelta(30)), # End 30 days from now
+		"TerminateInstancesWithExpiration": False,
+		"LaunchSpecifications":launch_specs
+	}
+	
+#	Check by exporting to JSON
+#	import json
+#	with open('test_config.json', 'w') as outfile:
+#		json.dump(SpotFleetRequestConfig, outfile,indent=4)
+	
+	print "requesting spot fleet..."
+	client.request_spot_fleet(DryRun=dry_run,SpotFleetRequestConfig=SpotFleetRequestConfig)
+	print "Done"
+
+if __name__=='__main__':
+		### Just test a few instances at a time ###
+	#
+	#instances=['c3.2xlarge','c4.4xlarge','cc2.8xlarge','m3.large']
+	#instances=['c4.large','c4.xlarge','c4.2xlarge','c4.8xlarge']
+	#instances=['m4.xlarge','m4.2xlarge','m4.4xlarge','m4.10xlarge']
+	#instances=['m3.medium','m3.xlarge','m3.2xlarge','m4.large']
+	#instances=['c3.large','c3.xlarge','c3.4xlarge','c3.8xlarge']
+	#instances=['r3.large','r3.xlarge','r3.2xlarge','r3.4xlarge','r3.8xlarge']
+	#instances=['i2.xlarge','i2.2xlarge','i2.4xlarge','i2.8xlarge']
+	#instances=['cr1.8xlarge','hi1.4xlarge']
+	
+	# Choose 12 cheapest instance types at time (by price per year run)
+	instances=[
+	'm4.4xlarge',
+	'c4.large',
+	'm4.large',
+	'c4.xlarge',
+	'm4.2xlarge',
+	'm4.xlarge',
+	'c4.2xlarge',
+	'c3.large',
+	'm3.large',
+	'c4.4xlarge',
+	'm3.2xlarge',
+	'r3.large']
+	launch_fleet(instances,4,'aws_bootscript_multiproc.sh',False)
